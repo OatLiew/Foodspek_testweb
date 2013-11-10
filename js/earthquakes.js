ad9d65cc47e6a9db4ce48684c2f0065947ccd65e
Phongsakorn <iphong@uw.edu>
2013-11-10 03:37:07 -0800

    all new

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -16,7 +16,7 @@ gov.usgs = gov.usgs || {};
 //the SODA api supports the cross-origin resource sharing HTTP header
 //so we should be able to request this URL from any domain via AJAX without
 //having to use the JSONP technique
-gov.usgs.quakesUrl = 'https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi';
+gov.usgs.quakesUrl = 'http://data.kingcounty.gov/resource/f29f-zza5.json';
 
 //current earthquake dataset (array of objects, each representing an earthquake)
 gov.usgs.quakes;
@@ -31,7 +31,17 @@ $(document).ajaxError(function(event, jqXHR, err){
 });
 
 $(function(){
+	
 	$('.message').html('Loading... <img src="img/loading.gif">');
+
+	// $('select[name="refer"]').change(function(){
+ //        //get a ref to the refer select
+ //        //add the refer-other input
+ //        var referSelect = $(this);
+ //        var test = referSelect.val();
+ //        alert("dasd");
+ //    }); //refer change function
+
 	getQuakes();
 
 });
@@ -47,12 +57,12 @@ function getQuakes(){
 	    //so we can reference it later in another event
 	    gov.usgs.quakes = quakes;
 
-	    $('.message').html('Display ' + quakes.length + ' earthquakes');
+	    $('.message').html('Display ' + quakes.length + ' datas');
 
 	    gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
-	    center: new google.maps.LatLng(0,0),        //centered on 0/0
-	    zoom: 2,                                    //zoom level 2
-	    mapTypeId: google.maps.MapTypeId.TERRAIN,   //terrain map
+	    center: new google.maps.LatLng(47.657901,-122.310797),        //centered on 0/0
+	    zoom: 14,                                    //zoom level 2
+	    mapTypeId: google.maps.MapTypeId.ROADMAP  ,   //terrain map
 	    streetViewControl: false                    //no street view
 		});
 
@@ -67,54 +77,59 @@ function addQuakeMarkers(quakes, map){
 	var quake;
 	var idx;
 	var infoWindow;
+	var date;
 
-	for(idx = 0; idx < 20; ++idx){	
+	// skip the first one for now
+	for(idx = 0; idx < quakes.length; ++idx){	
 		quake = quakes[idx];
 
-		if(quake.location){
-			//assuming that the variable 'quake' is set to 
-			//the current quake object within the quakes array...
-			quake.mapMarker = new google.maps.Marker({
-			    map: map,
-			    raiseOnDrag: true,
-			    animation: google.maps.Animation.DROP,
-			    position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
-			});
-		
-			infoWindow = new google.maps.InfoWindow({
-		    content: new Date(quake.datetime).toLocaleString() + 
-		                ': magnitude ' + quake.magnitude + ' at depth of ' + 
-		                quake.depth + ' meters'
-			});
-			registerInfoWindow(map, quake.mapMarker, infoWindow);
+			if(quake.latitude){
+				//assuming that the variable 'quake' is set to 
+				//the current quake object within the quakes array...
+					quake.mapMarker = new google.maps.Marker({
+				    map: map,
+				    raiseOnDrag: true,
+				    //animation: google.maps.Animation.DROP,
+				    position: new google.maps.LatLng(quake.latitude, quake.longitude)
+				});
 
 
-		}
+			
+				infoWindow = new google.maps.InfoWindow({
+			    content: new Date(quake.inspection_date).toLocaleString() + 
+			                ' Name: ' + quake.inspection_business_name + ' Inspection score:' + 
+			                quake.inspection_score + ' Violation_type: ' + quake.violation_type
+			                + ' Violation_description:' + quake.violation_description
+				});
+
+
+				registerInfoWindow(map, quake.mapMarker, infoWindow);
+			}
 	}
 
-	for(idx = 20; idx < quakes.length; ++idx){	
-		quake = quakes[idx];
+	// for(idx = 20; idx < quakes.length; ++idx){	
+	// 	quake = quakes[idx];
 
-		if(quake.location){
-			//assuming that the variable 'quake' is set to 
-			//the current quake object within the quakes array...
-			quake.mapMarker = new google.maps.Marker({
-			    map: map,
-			    raiseOnDrag: true,
-			    //animation: google.maps.Animation.DROP,
-			    position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
-			});
+	// 	if(quake.location){
+	// 		//assuming that the variable 'quake' is set to 
+	// 		//the current quake object within the quakes array...
+	// 		quake.mapMarker = new google.maps.Marker({
+	// 		    map: map,
+	// 		    raiseOnDrag: true,
+	// 		    //animation: google.maps.Animation.DROP,
+	// 		    position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+	// 		});
 
-			infoWindow = new google.maps.InfoWindow({
+	// 		infoWindow = new google.maps.InfoWindow({
 
-		    content: new Date(quake.datetime).toLocaleString() + 
-		                ': magnitude ' + quake.magnitude + ' at depth of ' + 
-		                quake.depth + ' meters'
-			});
-			registerInfoWindow(map, quake.mapMarker, infoWindow);
+	// 	    content: new Date(quake.datetime).toLocaleString() + 
+	// 	                ': magnitude ' + quake.magnitude + ' at depth of ' + 
+	// 	                quake.depth + ' meters'
+	// 		});
+	// 		registerInfoWindow(map, quake.mapMarker, infoWindow);
 
-		}
-	}
+	// 	}
+	// }
 }
 
 function registerInfoWindow(map, marker, infoWindow) {
